b9f630ca16e09397c027b30e3cb1fd210b356e4c
Kit0b0y <revizor@me.com>
2022-08-23 10:15:31 +0300

    Update main.go

--- Temp/main.go
+++ Temp/main.go
@@ -1,26 +1,25 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
-	var n int = 1
-	fact := 1
-	var x float64 = 1
-	var ex float64
-	var pex float64 = 0
-	epsilon := 0.1
-
-	for {
-		for i := 1; i <= n; i++ {
-			fact *= i
-		}
-		ex += math.Pow(x, float64(n)) / float64(fact)
-		if math.Abs(ex-pex) < epsilon {
-			println(ex)
-			break
-		}
-		n++
-		pex = ex
+	s1 := "Go lang"
+	s2 := "go lang"
+	count := 0
+	a1 := strings.Split(s1, " ")
+	a2 := strings.Split(s2, " ")
+	fmt.Println(a1)
+	fmt.Println(a2)
+	space := " "
+	spaceIndex := strings.Index(s1, space)
+	w1 := a1[spaceIndex:]
+	w2 := a2[spaceIndex:]
+	if strings.Contains(w1, w2) {
+		count++
+		fmt.Println(count)
 	}
 }
