2aa34c812e1f0e9a09e7f3b96c488b56af413299
Kit0b0y <revizor@me.com>
2022-08-27 13:10:15 +0300

    Task

--- Temp/main.go
+++ Temp/main.go
@@ -2,24 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
 func main() {
-
-	s1 := "Go lang"
-	s2 := "go lang"
-	count := 0
-	a1 := strings.Split(s1, " ")
-	a2 := strings.Split(s2, " ")
-	fmt.Println(a1)
-	fmt.Println(a2)
-	space := " "
-	spaceIndex := strings.Index(s1, space)
-	w1 := a1[spaceIndex:]
-	w2 := a2[spaceIndex:]
-	if strings.Contains(w1, w2) {
-		count++
-		fmt.Println(count)
+	switch len(os.Args) {
+	case 2:
+		inFileA := os.Args[1]
+		printFileContent(&inFileA)
+	case 3:
+		inFileA := os.Args[1]
+		inFileB := os.Args[2]
+		content := joinFilesContent(&inFileA, &inFileB)
+		fmt.Printf("<%s> + <%s> content: <%s>", inFileA, inFileB, content)
+	case 4:
+		inFileA := os.Args[1]
+		inFileB := os.Args[2]
+		outFile := os.Args[3]
+		content := joinFilesContent(&inFileA, &inFileB)
+		err := ioutil.WriteFile(outFile, []byte(content), 0644)
+		check(err)
+		fmt.Printf("Содержимое файла <%s> заполнено сконкатенированным содержанием файлов <%s> и <%s>", outFile, inFileA, inFileB)
+	default:
+		fmt.Println("Программу необходимо запустить с аргументами командной строки!")
+		return
 	}
+}
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func joinFilesContent(fileName1, fileName2 *string) string {
+	file1Content, err := ioutil.ReadFile(*fileName1)
+	check(err)
+
+	file2Content, err := ioutil.ReadFile(*fileName2)
+	check(err)
+
+	return strings.Join([]string{string(file1Content), string(file2Content)}, "; ")
+}
+
+func printFileContent(fileName *string) {
+	content, err := ioutil.ReadFile(*fileName)
+	check(err)
+	fmt.Printf("<%s> content: <%s>", *fileName, string(content))
 }
